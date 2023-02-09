2f71b842ed1f5ae51701f1ff53160ae194585456
Minami Tomonobu <minami@edanoue.com>
2023-02-09 12:17:16 +0900

    :sparkles: 読み取り/書き込み専用の Accessor のインタフェースを追加

---
+++ Runtime/Interfaces/IReadOnlyEdaFeatureAccessor.cs
@@ -0,0 +1,19 @@
+// Copyright Edanoue, Inc. All Rights Reserved.
+
+#nullable enable
+
+namespace Edanoue.ComponentSystem
+{
+    /// <summary>
+    /// <para><see cref="EdaFeatureCollector" /> に所属することができるアクセサーに求められるインタフェース.</para>
+    /// <para>Feature の登録のみが可能</para>
+    /// </summary>
+    public interface IReadOnlyEdaFeatureAccessor : IEdaFeatureAccessor
+    {
+        void IEdaFeatureAccessor.AddFeatures(IWriteOnlyEdaFeatureCollector collector)
+        {
+            // (南) ここで明示的実装をしておくことで擬似的に AddFeatures の実装の必要がない
+            // みたいにしていますが, やろうと思ったら別にかけるのでビミョーです
+        }
+    }
+}
---
+++ Runtime/Interfaces/IWriteOnlyEdaFeatureAccessor.cs
@@ -0,0 +1,19 @@
+// Copyright Edanoue, Inc. All Rights Reserved.
+
+#nullable enable
+
+namespace Edanoue.ComponentSystem
+{
+    /// <summary>
+    /// <para><see cref="EdaFeatureCollector" /> に所属することができるアクセサーに求められるインタフェース.</para>
+    /// <para>Feature の登録のみが可能</para>
+    /// </summary>
+    public interface IWriteOnlyEdaFeatureAccessor : IEdaFeatureAccessor
+    {
+        void IEdaFeatureAccessor.GetFeatures(IReadOnlyEdaFeatureCollector collector)
+        {
+            // (南) ここで明示的実装をしておくことで擬似的に GetFeature の実装の必要がない
+            // みたいにしていますが, やろうと思ったら別にかけるのでビミョーです
+        }
+    }
+}
