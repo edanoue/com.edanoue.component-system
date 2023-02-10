f0d70e3db89aaa9ccfb3e66954f6106aea45737d
Minami Tomonobu <minami@edanoue.com>
2023-02-10 20:43:44 +0900

    :sparkles: MonoBehaviour の Accessor を収集するスクリプトを追加

---
+++ Runtime/EdaFeatureAccessorCollector.cs
@@ -0,0 +1,41 @@
+// Copyright Edanoue, Inc. All Rights Reserved.
+
+#nullable enable
+using System;
+using UnityEngine;
+
+namespace Edanoue.ComponentSystem
+{
+    public class EdaFeatureAccessorCollector : MonoBehaviour
+    {
+        [SerializeField]
+        private Target m_collectTarget;
+
+        private void Awake()
+        {
+            switch (m_collectTarget)
+            {
+                case Target.SiblingOnly:
+                {
+                    var mbAccessor = GetComponents<IEdaFeatureAccessor>();
+                    EdaFeatureCollector.Create(mbAccessor);
+                    break;
+                }
+                case Target.AllDescendants:
+                {
+                    var mbAccessor = GetComponentsInChildren<IEdaFeatureAccessor>();
+                    EdaFeatureCollector.Create(mbAccessor);
+                    break;
+                }
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private enum Target
+        {
+            SiblingOnly,
+            AllDescendants
+        }
+    }
+}
